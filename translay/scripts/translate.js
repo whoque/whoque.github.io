d8adbfa93c7dfdc4cebf657c357e7f602a2c899e
whoque16@gmail.com <whoque16@gmail.com>
2020-04-08 03:23:57 +0530

    test6

--- translay/scripts/translate.js
+++ translay/scripts/translate.js
@@ -1,7 +1,8 @@
 function translateFunc() {
   var inputval = document.getElementById("reconText").value;
   var voices = window.speechSynthesis.getVoices();
-  var language = voices[$("#voices").val()].lang.split("-")[0];
+  var languageCode = voices[$("#voices").val()].lang;
+  var language = languageCode.split("-")[0];
   let url =
     "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170415T074852Z.f30908e49daf053e.1533851723fbfa2f832c94c5bf992441d9f76604&lang=" +
     language +
